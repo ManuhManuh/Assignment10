880b7e2fc83c5f386b4119c28edd77144a6e6374
ManuhManuh <cara.sterling200@gmail.com>
2022-07-11 03:00:47 -0600

    Refactor
    Created more methods, renamed variables, eliminated magic numbers, moved cursor lock from update to start

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,34 +7,52 @@ namespace LongMethod
 {
     public class CharacterController : MonoBehaviour
     {
+        [SerializeField] private float jumpSpeed = 10;
+        [SerializeField] private float positionModifier = 7;
+        [SerializeField] private float rotationModifier = 45;
+        [SerializeField] private float minCameraRotation = 10;
+        [SerializeField] private float maxCameraRotation = 35;
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
         void Update()
         {
-            // Keeps cursor locked in game window
-            Cursor.lockState = CursorLockMode.Locked;
+            MoveCharacter();
+            RotateCamera();
+        }
 
-            var mov = (Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward) .normalized;
-            transform.position += mov * Time.deltaTime * 7;
+        private void RotateCamera()
+        {
+            var mainCamera = transform.Find("Main Camera").transform;
+            var xRotation = mainCamera.localRotation.eulerAngles.x - rotationModifier * Input.GetAxis("Mouse Y") * Time.deltaTime;
+            if (xRotation > maxCameraRotation)
+            {
+                xRotation = maxCameraRotation;
+            }
+            else if (xRotation < minCameraRotation)
+            {
+                xRotation = minCameraRotation;
+            }
+            mainCamera.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        }
+
+        private void MoveCharacter()
+        {
+            var movement = (Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward).normalized;
+            transform.position += positionModifier * Time.deltaTime * movement;
+            transform.localRotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + rotationModifier *
+                                                                                 Input.GetAxis("Mouse X") * Time.deltaTime, 0);
 
             if (Input.GetButtonDown("Jump"))
-                GetComponent<Rigidbody>().velocity = Vector3.up * 10;
-
-
-
-            transform.localRotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 45 * Input.GetAxis("Mouse X") * Time.deltaTime, 0);
-
-            var t = transform.Find("Main Camera").transform;
-            var xRot = t.localRotation.eulerAngles.x - 45 * Input.GetAxis("Mouse Y") * Time.deltaTime;
-            if (xRot > 35) xRot = 35; else if (xRot < 10) xRot = 10;
-            t.localRotation = Quaternion.Euler(xRot, 0, 0);
-
-
+                Jump();
         }
 
-        void Awake()
+        private void Jump()
         {
-
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpSpeed;
         }
 
-        void Start() {}
     }
 }
